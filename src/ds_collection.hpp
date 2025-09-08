cd77d48023da4e33f4632f6c5bdb4ee6f4b3f183
MingMinNa <n139726845@gmail.com>
2025-09-09 00:11:13 +0800

    Refactor: PQ -> heap

--- src/ds_collection.hpp
+++ src/ds_collection.hpp
@@ -5,7 +5,7 @@
 #include "queue/queue.hpp"
 #include "linked_list/linked_list.hpp"
 #include "DSU/disjoint_set.hpp"
-#include "PQ/priority_queue.hpp"
+#include "heap/heap.hpp"
 #include "MMH/mmh.hpp"
 #include "SMMH/smmh.hpp"
 #include "deque/deque.hpp"
